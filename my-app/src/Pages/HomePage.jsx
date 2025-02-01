bb70897da6251ebe6edad931a33540b80054ebee
kanishkapan <147300802+kanishkapan@users.noreply.github.com>
2025-02-01 20:19:51 +0530

    Home page Going on

---
+++ my-app/src/Pages/HomePage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import HeroSection from '../components/HomePage/HeroSection'
+import MedicalServices from '../components/HomePage/MedicalServices'
+import HealthcareHeader from '../components/HomePage/HealthcareHeader'
+import SpecialistCard from '../components/HomePage/SpecialistCard'
+
+const HomePage = () => {
+  return (
+    <div>
+
+
+<HeroSection/>
+<MedicalServices/>
+<HealthcareHeader/>
+<SpecialistCard/>
+
+
+
+
+    </div>
+  )
+}
+
+export default HomePage
