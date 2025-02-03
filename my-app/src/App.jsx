bc49f70fd1fcf87e7112eeb959a5295df6c5b4e5
kanishkapan <147300802+kanishkapan@users.noreply.github.com>
2025-02-03 19:45:24 +0530

    Contact Page done

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,10 +4,11 @@ import Navbar from "../src/components/Navbar/Navbar";
 import HomePage from "./Pages/HomePage";
 import PatientForm from "./Pages/PatientForm";
 import Aibot from "./Pages/Aibot";
-
+import Booking from "./Pages/Booking";
+import Contact from "./Pages/Contact";
 const Home = () => <div className="text-center mt-10">🏠 Welcome to Home</div>;
 const AIBot = () => <div className="text-center mt-10">🤖 AI Bot Page</div>;
-const Contact = () => <div className="text-center mt-10">📞 Contact Page</div>;
+
 const Profile = () => <div className="text-center mt-10">👤 Profile Page</div>;
 const Appointment = () => <div className="text-center mt-10">📅 Appointment Page</div>;
 const VideoCall = () => <div className="text-center mt-10">📹 Video Call Page</div>;
@@ -23,7 +24,7 @@ const App = () => {
         <Route path="/ai-bot" element={<Aibot />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/appointment" element={<Booking />} />
         <Route path="/video-call" element={<VideoCall />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
